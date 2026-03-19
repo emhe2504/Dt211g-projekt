4d9903cc530cbe3bf920d56062369759586ae260
Emma Heikkinen <emhe2504@student.miun.se>
2026-03-19 09:33:54 +0100

    jsDoc

---
+++ src/js/nextLastFetch-doc.js
@@ -0,0 +1,157 @@
+
+/**
+ * Använder namn på huvudstad och
+ * tar fram API med aktuell tidsinformation från huvudstaden.
+ * @param {string} capital 
+ * @returns {Promise<void>}
+ */
+async function getTime(capital) {
+
+    const APIkey = "7N7ARBIB953X";
+    const link = `https://api.timezonedb.com/v2.1/get-time-zone?key=${APIkey}&format=json&by=zone&zone=Europe/${capital}`;
+
+    
+    try {
+
+        const linkData = await fetch(link);
+        const jsonData = await linkData.json();
+
+        sendTime(jsonData);
+
+    } catch(error) {
+
+        console.log(error);
+    }
+}
+
+/**
+ * Tar aktuell tid samt tidszon från timeZone API
+ * och skickar till addTime.
+ * @param {Object} jsonData 
+ */
+
+function sendTime(jsonData) {
+
+    const dateTime = jsonData.formatted;
+    const zoneName = jsonData.zoneName;
+    const abbreviation = jsonData.abbreviation;
+
+    /*Ta fram tid och tidszon, skicka vidare */
+
+    addTime(dateTime, zoneName, abbreviation);
+}
+
+/**
+ * Skriver ut tid och tidszon till DOM.
+ * @param {string} dateTime - aktuell tid och datum
+ * @param {string} zoneName - namn på tidszon
+ * @param {string} abbreviation - förkortning av tidszon
+ */
+
+function addTime(dateTime, zoneName, abbreviation) {
+
+    const timeSpot = document.getElementById("timeSpot");
+
+    timeSpot.innerHTML = `<p>Date and time in capital: ${dateTime}</p>`;
+
+    const timeZoneSpot = document.createElement("p");
+    timeZoneSpot.textContent = `Timezone: ${zoneName}, ${abbreviation}`;
+    timeSpot.appendChild(timeZoneSpot);
+
+    if(!dateTime) {
+
+        timeSpot.innerHTML = `<p>Date and time in capital: Can't find time</p>`;
+        timeZoneSpot.textContent = `Timezone: Can't find timezone`;
+    }
+
+       if(!zoneName || !abbreviation) {
+
+        timeZoneSpot.textContent = `Timezone: Can't find timezone`;
+        timeSpot.appendChild(timeZoneSpot);
+    }
+}
+
+
+
+
+/**
+ * Hämtar aktuell väderinformation för en huvudstad 
+ * via openweathermap API.
+ * @param {string} capital 
+ * @returns {Promise<void>}
+ */
+
+async function getWeather(capital) {
+
+    const APIkey = "ee3605817a74d55e0c0111a618e04922";
+    const link = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${APIkey}&units=metric`;
+
+
+    try {
+
+        const linkData = await fetch(link);
+        const jsonData = await linkData.json();
+
+        sendWeather(jsonData, capital);
+
+    } catch (error) {
+
+        console.log(error);
+    }
+}
+
+/**
+ * Tar aktuell temp, väder, väderbeskrivning och
+ * väder-icon från openweathermap API
+ * och skickar till addWeather. Skickar även
+ * aktuell huvudstad.
+ * @param {Object} jsonData
+ * @param {string} capital 
+ */
+
+function sendWeather(jsonData, capital) {
+
+    const temp = jsonData.main.temp;
+    const weather = jsonData.weather[0].main;
+    const description = jsonData.weather[0].description;
+    const icon = jsonData.weather[0].icon;
+    const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+    addWeather(temp, description, weather, iconURL, capital);
+}
+
+/**
+ * Skriver temp, väder, väderbeskrivning och
+ * väder-icon till DOM.
+ * @param {number} temp 
+ * @param {string} description 
+ * @param {string} weather 
+ * @param {string} iconURL 
+ * @param {string} capital 
+ */
+
+function addWeather(temp, description, weather, iconURL, capital) {
+
+    const tempSpot = document.getElementById("tempSpot");
+
+    tempSpot.innerHTML = `<p>Temperature in ${capital}: ${temp} degrees.</p>`;
+
+    const weatherSpot = document.createElement("p");
+    weatherSpot.textContent = `Weather right now: ${weather}, ${description}.`;
+    tempSpot.appendChild(weatherSpot);
+
+    const img = document.createElement("img");
+    img.src = iconURL;
+    img.alt = `Weather-icon`;
+    tempSpot.appendChild(img);
+
+    if (temp === null) {
+
+        tempSpot.innerHTML = `<p>Weather in capital: Can't find weather</p>`;
+    }
+
+    if (!description || !weather) {
+
+        weatherSpot.textContent = `Weather right now: Missing info`;
+    }
+}
---
+++ src/js/searchFetch-doc.js
@@ -0,0 +1,233 @@
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+/** @type {HTMLInputElement} */
+const search = document.getElementById("search-spot"); //Sökruta
+
+/** @type {HTMLDivElement | null} */
+const wrong = document.getElementById("wrong"); //Div med felmeddelande
+
+
+/**
+ * Anropar searchInfo och closeInfo.
+ * Eventlyssnare som vid input i textruta 
+ * döljer felmeddelande.
+ */
+function init() {
+    searchInfo();
+    closeInfo();
+
+    if (wrong) {
+    search.addEventListener("input", () => {
+        wrong.classList.add("is_hidden");
+    });
+}
+}
+
+
+
+
+/**
+ * Tar värdet som skrivs in i sökruta (value),
+ * formaterar texten (stor första bokstav i alla ord),
+ * skickar vidare till första funktionen för fetch (getInfo)
+ * vid knapptryck.
+ */
+
+function searchInfo() {
+
+    const searchPlace = document.getElementById("search-spot");
+    const button = document.getElementById("search-button");
+
+    button.addEventListener("click", () => {
+
+        /*Ta ordet/orden i value, dela upp, stor första bokstav, resten små, sätt ihop */
+        
+        const value = searchPlace.value
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+            .join(' ');
+            
+        getInfo(value);
+        searchPlace.value = "";
+    });
+}
+
+
+
+
+
+/**
+ * Tar emot sökord från sökruta och skickar vidare till (printInfo).
+ * Fetchar landdata från API, samt skickar data från API
+ * till funktion (printInfo)
+ * @param {string} value - Sökord från sökruta
+ * @returns {Promise<void>}
+ */
+
+async function getInfo(value) {
+
+    const link = `https://restcountries.com/v3.1/region/europe?fields=name,capital,flags,languages,currencies,population`;
+
+    try {
+        const linkData = await fetch(link);
+        const jsonData = await linkData.json();
+
+        printInfo(jsonData, value);
+    }
+
+    catch (error) {
+        console.log(error);
+
+        const errorSpot = document.getElementById("result-section");
+
+        errorSpot.innerHTML = `
+        <p>Problem att ladda informationen, prova gärna igen</p>`
+    }
+}
+
+/**
+ * Deklarera olika variabler för platser 
+ * */
+
+/** @type {HTMLElement} */
+const infoSpot = document.getElementById("result-section"); // section för div:ar
+
+/** @type {HTMLDivElement} */
+const nameSpot = document.getElementById("nameSpot"); // div för landnamn
+
+/** @type {HTMLDivElement} */
+const flagSpot = document.getElementById("flagSpot"); // div för flagga
+
+/** @type {HTMLDivElement} */
+const restSpot = document.getElementById("restOfInfo"); // div för resterande landinfo
+
+/** @type {HTMLElement} */
+const destinations = document.getElementById("destination-section"); // section med reseförslag
+
+/** @type {HTMLDivElement} */
+const start = document.getElementById("start-section"); // div med starttext
+
+
+
+/**
+ * Visar information om land utifrån användarens sökning
+ * och restcountries API.
+ * Skriver ut landnamn, flagga, huvudstad, språk, valuta
+ * och population till skärmen.
+ * Skickar huvudstad till funktioner med fetch för tid och väder.
+ * @param {Object[]} jsonData - landdata från restcountries API
+ * @param {string} value - sökord från sökruta
+ */
+
+function printInfo(jsonData, value) {
+
+    const countryInfo = jsonData.find(data => data.name.common === value);
+
+    if (countryInfo) {
+        const country = countryInfo.name.common;
+
+        flagSpot.innerHTML = "";
+        restSpot.innerHTML = "";
+
+        infoSpot.classList.remove("is_hidden");
+
+        if (!infoSpot.classList.contains("is_hidden")) {
+            destinations.classList.add("is_hidden");
+            infoSpot.scrollIntoView();
+        }
+
+
+        /*Skriv ut land */
+
+        nameSpot.innerHTML = `<h3>${country}</h3>`;
+
+
+        /*Skriv ut flagga */
+
+        const flag = countryInfo.flags.png;
+        const img = document.createElement("img");
+        img.src = flag;
+        img.alt = `Flag of ${country}`;
+        flagSpot.appendChild(img);
+
+
+        /*Skriv ut huvudstad */
+
+        const capital = countryInfo.capital[0];
+        const capitalSpot = document.createElement("p");
+        capitalSpot.textContent = `Capital: ${capital}`;
+        restSpot.appendChild(capitalSpot);
+
+        getTime(capital);   /*Anger stad till annat API för tid i huvudstad */
+        getWeather(capital); /*Anger stad till annat API för väder huvudstad */
+
+
+        /*Skriv ut språk */
+
+        const languages = countryInfo.languages;
+        const langName = Object.values(languages).join(", ");
+
+        const languageSpot = document.createElement("p");
+        languageSpot.textContent = `Language/Languages: ${langName}`;
+        restSpot.appendChild(languageSpot);
+
+
+        /*Skriv ut valuta */
+
+        const currencies = countryInfo.currencies;
+        const currName = Object.values(currencies);
+
+        currName.forEach(curr => {
+            const currancy = curr.name;
+            const symbol = curr.symbol;
+
+            const currencySpot = document.createElement("p");
+            currencySpot.textContent = `Currency: ${currancy}, (Symbol: ${symbol})`;
+            restSpot.appendChild(currencySpot);
+        })
+
+
+        /*Skriv ut population */
+
+        const population = countryInfo.population.toLocaleString();
+        const popSpot = document.createElement("p");
+        popSpot.textContent = `Population: ${population} people`;
+        restSpot.appendChild(popSpot);
+
+    } else {
+
+        /*Om skriver fel eller inte kan hitta land */
+
+        const wrong = document.getElementById("wrong");
+
+        wrong.classList.remove("is_hidden");
+        infoSpot.classList.add("is_hidden");
+
+        if (infoSpot.classList.contains("is_hidden")) {
+            destinations.classList.remove("is_hidden");
+        }
+
+    }
+}
+
+/**
+ * Skapar eventlyssnare på stäng-kanppen,
+ * döljer visad landinformation.
+ */
+
+function closeInfo() {
+
+    const closeButton = document.getElementById("close");
+
+    closeButton.addEventListener("click", () => {
+
+        infoSpot.classList.toggle("is_hidden");
+
+        if (infoSpot.classList.contains("is_hidden")) {
+            destinations.classList.remove("is_hidden");
+            start.scrollIntoView();
+        }
+    })
+}
